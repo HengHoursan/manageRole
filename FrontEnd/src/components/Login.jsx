74e393fbf09027038ecb3c44fe225aee0c41401d
HengHoursan <157660651+HengHoursan@users.noreply.github.com>
2026-02-13 14:52:26 +0700

    updated bot username

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -80,9 +80,7 @@ const Login = () => {
       script.id = "telegram-widget-script";
       script.src = "https://telegram.org/js/telegram-widget.js?22";
       script.async = true;
-
-      // ATTENTION: Change 'first_test_bot' to your actual bot username
-      script.setAttribute("data-telegram-login", "first_test_bot");
+      script.setAttribute("data-telegram-login", "SarnaBot_bot");
       script.setAttribute("data-size", "large");
       script.setAttribute("data-radius", "8");
       script.setAttribute("data-onauth", "onTelegramAuth(user)");
