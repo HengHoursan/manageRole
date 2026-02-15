b2fa0a658c1c08d5d18cbe65edf8cb4947557ad3
HengHoursan <157660651+HengHoursan@users.noreply.github.com>
2026-02-15 16:33:45 +0700

    changed bot

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -82,7 +82,7 @@ const Login = () => {
     const script = document.createElement("script");
     script.src = "https://telegram.org/js/telegram-widget.js?22";
     script.async = true;
-    script.setAttribute("data-telegram-login", "SarnaBot_bot");
+    script.setAttribute("data-telegram-login", "second_test1_bot");
     script.setAttribute("data-size", "large");
     script.setAttribute("data-radius", "8");
     script.setAttribute("data-onauth", "onTelegramAuth(user)");
