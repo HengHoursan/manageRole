1d6cba97a19e468956aa1ada2237b0b9cf714088
HengHoursan <157660651+HengHoursan@users.noreply.github.com>
2026-02-16 14:43:42 +0700

    updated

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -86,7 +86,10 @@ const Login = () => {
     script.id = "telegram-login-script";
     script.src = "https://telegram.org/js/telegram-widget.js?22";
     script.async = true;
-    script.setAttribute("data-telegram-login", enviroment.telegram_bot_name);
+    script.setAttribute(
+      "data-telegram-login",
+      import.meta.env.VITE_TELEGRAM_BOT_NAME,
+    );
     script.setAttribute("data-size", "large");
     script.setAttribute("data-onauth", "handleTelegramAuth(user)");
     script.setAttribute("data-request-access", "write");
