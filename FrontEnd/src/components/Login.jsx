0f58cbda5e0cd2cfd77626916ccc9a787b33bbdc
HengHoursan <157660651+HengHoursan@users.noreply.github.com>
2026-02-16 15:37:26 +0700

    updated tt

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -83,7 +83,9 @@ const Login = () => {
       script.id = "telegram-login-script";
       script.src = "https://telegram.org/js/telegram-widget.js?22";
       script.async = true;
-      script.setAttribute("data-telegram-login", "second_test1_bot");
+      // Change this:
+      script.setAttribute("data-telegram-login", "second_test_1_bot"); // Added the underscore
+      // script.setAttribute("data-telegram-login", "second_test1_bot");
       // TODO: replace with import.meta.env.VITE_TELEGRAM_BOT_NAME
       script.setAttribute("data-size", "large");
       script.setAttribute("data-onauth", "onTelegramAuth(user)");
