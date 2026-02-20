b86140ba4e7596945ce92ccfeab6ac068875fcd1
HengHoursan <157660651+HengHoursan@users.noreply.github.com>
2026-02-20 17:32:17 +0700

    updated mini app

--- BackEnd/src/routes/authentication.routes.js
+++ BackEnd/src/routes/authentication.routes.js
@@ -11,4 +11,10 @@ router.post("/login", authenicationController.login);
 // telegram login route (widget-based - kept as fallback)
 router.post("/telegram-login", authenicationController.telegramLogin);
 
+// telegram mini app login route
+router.post(
+  "/telegram-webapp-login",
+  authenicationController.telegramWebAppLogin,
+);
+
 module.exports = router;
